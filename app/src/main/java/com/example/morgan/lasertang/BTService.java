611bffa662e7c5a6df27ea6c69401d76ed300051
niggor <zotov.bmstu@gmail.com>
2016-04-08 14:35:38 +0300

    bt works via service. fuck yeah

--- app/src/main/java/com/example/morgan/lasertang/BTService.java
+++ app/src/main/java/com/example/morgan/lasertang/BTService.java
@@ -1,173 +1,1 @@
-package com.example.morgan.lasertang;
-
-import android.app.Service;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.IBinder;
-import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.Toast;
-
-import java.io.IOException;
-import java.io.OutputStream;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Set;
-
-public class BTService extends Service {
-
-    private static final int REQUEST_ENABLE_BT = 1;
-    // Tank Commands
-    private static final byte CMD_SHOOT = 0x01;
-    private static final byte CMD_MOVE = 0x02;
-
-    BluetoothSocket clientSocket;
-    private BroadcastReceiver mReceiver = null;
-    BluetoothAdapter bluetooth;
-
-    ArrayAdapter<String> itemsAdapter;
-
-    public BTService() {
-    }
-
-    @Override
-    public IBinder onBind(Intent intent) {
-
-        Log.d(LOG_TAG, "onBind");
-//        return null;
-
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    final String LOG_TAG = "myLogs";
-
-    public void onCreate() {
-        super.onCreate();
-        Log.d(LOG_TAG, "onCreate");
-    }
-
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d(LOG_TAG, "onStartCommand");
-        someTask();
-        return super.onStartCommand(intent, flags, startId);
-    }
-
-    public void onDestroy() {
-        super.onDestroy();
-        unregisterReceiver(mReceiver);
-        Log.d(LOG_TAG, "onDestroy");
-    }
-
-    void someTask() {
-
-        Log.d(LOG_TAG, "SomeTask Function");
-
-        bluetooth = BluetoothAdapter.getDefaultAdapter();
-
-        Log.d(LOG_TAG, "bluetooth.isEnabled Function");
-
-//        if (!bluetooth.isEnabled()) {
-//            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-//            enableBtIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//            Log.d(LOG_TAG, "startService Function");
-//            startService(enableBtIntent);
-//        }
-
-        Log.d(LOG_TAG, "bluetooth.enable()");
-        bluetooth.enable();
-        registerReceiver(mReceiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
-
-        Log.d(LOG_TAG, "showBluetoothDevices Function");
-        showBluetoothDevices();
-
-//        // Register the BroadcastReceiver
-//        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-//        registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
-
-        Log.d(LOG_TAG, "TRY connection BLOCK");
-
-        try {
-            BluetoothDevice device = bluetooth.getRemoteDevice("20:15:02:11:26:45");
-            Method m = device.getClass().getMethod("createRfcommSocket", new Class[]{int.class});
-            clientSocket = (BluetoothSocket) m.invoke(device, 1);
-            clientSocket.connect();
-        } catch (IOException |
-                SecurityException |
-                IllegalArgumentException |
-                NoSuchMethodException |
-                InvocationTargetException |
-                IllegalAccessException e) {
-            Log.d("BLUETOOTH", e.getMessage());
-        }
-    }
-
-    private void showBluetoothDevices() {
-
-        if (!bluetooth.isEnabled()) {
-            return;
-        }
-
-        Set<BluetoothDevice> pairedDevices = bluetooth.getBondedDevices();
-        // If there are paired devices
-        if (pairedDevices.size() > 0) {
-            // Loop through paired devices
-            for (BluetoothDevice device : pairedDevices) {
-                // Add the name and address to an array adapter to show in a ListView
-                itemsAdapter.add(device.getName() + "\n" + device.getAddress());
-            }
-        }
-
-        boolean discovering = bluetooth.startDiscovery();
-        if (!discovering) {
-            //Toast.makeText(getApplicationContext(), "CANNOT DISCOVER", Toast.LENGTH_LONG).show();
-        } else {
-            // Create a BroadcastReceiver for ACTION_FOUND
-            mReceiver = new BroadcastReceiver() {
-                public void onReceive(Context context, Intent intent) {
-                    String action = intent.getAction();
-                    // When discovery finds a device
-                    if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                        // Get the BluetoothDevice object from the Intent
-                        BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                        // Add the name and address to an array adapter to show in a ListView
-                        itemsAdapter.add(device.getName() + "\n" + device.getAddress());
-                    }
-                }
-            };
-        }
-    }
-
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        // запишем в лог значения requestCode и resultCode
-//        Log.d("myLogs", "requestCode = " + requestCode + ", resultCode = " + resultCode);
-//        // если пришло ОК
-//        if (resultCode == RESULT_OK) {
-//            // show list of BT devices
-//            showBluetoothDevices();
-//        } else {
-//            Toast.makeText(this, "Wrong result", Toast.LENGTH_SHORT).show();
-//        }
-//    }
-
-    private boolean sendCMD(byte cmd) {
-        byte[] buf = {cmd};
-        return sendCMD(buf);
-    }
-
-    private boolean sendCMD(byte[] cmd) {
-        try {
-            OutputStream outStream = clientSocket.getOutputStream();
-            outStream.write(cmd);
-            return true;
-        } catch (IOException e) {
-            Log.d("BLUETOOTH", e.getMessage());
-        }
-        return false;
-    }
-}
+package com.example.morgan.lasertang;import android.app.Service;import android.bluetooth.BluetoothAdapter;import android.bluetooth.BluetoothDevice;import android.bluetooth.BluetoothSocket;import android.content.BroadcastReceiver;import android.content.Context;import android.content.Intent;import android.content.IntentFilter;import android.os.IBinder;import android.util.Log;import android.widget.ArrayAdapter;import java.io.IOException;import java.io.OutputStream;import java.util.HashMap;import java.util.List;import java.util.Set;public class BTService extends Service {    private boolean BTenabled = false;    private boolean BTwasEnabled = false;    private boolean scaningForDevices = false;    final String LOG_TAG = "BTSERVICE";    BluetoothSocket clientSocket;    BluetoothAdapter bluetooth;    HashMap<String, String> BTdevices;    public BTService() {    }    @Override    public IBinder onBind(Intent intent) {        Log.d(LOG_TAG, "onBind");        // TODO: Return the communication channel to the service.        throw new UnsupportedOperationException("Not yet implemented");    }    public void onCreate() {        super.onCreate();        Log.d(LOG_TAG, "onCreate");        BTdevices = new HashMap<String, String>();        IntentFilter filter1 = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);        registerReceiver(BCR_ActionChange, filter1);        IntentFilter filter2 = new IntentFilter();        filter2.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);        filter2.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);        filter2.addAction(BluetoothAdapter.ACTION_SCAN_MODE_CHANGED);        filter2.addAction(BluetoothDevice.ACTION_FOUND);        registerReceiver(BCR_FoundDevice, filter2);    }    public int onStartCommand(Intent intent, int flags, int startId) {        Log.d(LOG_TAG, "onStartCommand");        if (!BTenabled) {            BTenabled = true;            launchBluetooth();        }        return super.onStartCommand(intent, flags, startId);    }    public void onDestroy() {        super.onDestroy();        if(!BTwasEnabled){            bluetooth.disable();        }        unregisterReceiver(BCR_ActionChange);        unregisterReceiver(BCR_FoundDevice);        Log.d(LOG_TAG, "onDestroy");    }    private void launchBluetooth() {        Log.d(LOG_TAG, "SomeTask Function");        bluetooth = BluetoothAdapter.getDefaultAdapter();        Log.d(LOG_TAG, "LOG bluetooth.enable()");        if (bluetooth.isEnabled()) {            BTwasEnabled = true;            showPairedDevices();            scanForDevices();        } else {            BTwasEnabled = false;            Log.d(LOG_TAG, "START INTENT ACTION_REQUEST_ENABLE");            boolean btEn = bluetooth.enable();            Log.d(LOG_TAG, "bluetooth.enable()->"+btEn);        }    }    private final BroadcastReceiver BCR_ActionChange = new BroadcastReceiver() {        @Override        public void onReceive(Context context, Intent intent) {            final String action = intent.getAction();            if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);                switch(state) {//                    case BluetoothAdapter.STATE_TURNING_OFF: break;//                    case BluetoothAdapter.STATE_TURNING_ON: break;                    case BluetoothAdapter.STATE_OFF:                        Log.d("BCR_ActionChange", "STATE_OFF");                        break;                    case BluetoothAdapter.STATE_ON:                        Log.d("BCR_ActionChange", "STATE_ON");                        showPairedDevices();                        boolean discovering = bluetooth.startDiscovery();                        Log.d(LOG_TAG, "bluetooth.startDiscovery()->"+discovering);                        break;                }            }        }    };    private void showPairedDevices() {        Log.d(LOG_TAG, "STATE_ON");        Set<BluetoothDevice> pairedDevices = bluetooth.getBondedDevices();        if (pairedDevices.size() > 0) {            for (BluetoothDevice device : pairedDevices) {                Log.d(LOG_TAG, device.getName() + "\n" + device.getAddress());                BTdevices.put(device.getName(), device.getAddress());            }        }    }    private final BroadcastReceiver BCR_FoundDevice = new BroadcastReceiver() {        @Override        public void onReceive(Context context, Intent intent) {            final String action = intent.getAction();            if (BluetoothDevice.ACTION_FOUND.equals(action)) {                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);                Log.d(LOG_TAG, device.getName() + "\n" + device.getAddress());                BTdevices.put(device.getName(), device.getAddress());            }        }    };    public boolean scanForDevices() {        if (scaningForDevices) {            return false;        } else {            if (bluetooth.isEnabled()) {                boolean discovering = bluetooth.startDiscovery();                Log.d(LOG_TAG, "bluetooth.startDiscovery()->" + discovering);                return true;            } else {                return false;            }        }    }    public boolean sendCMD(byte cmd) {        byte[] buf = {cmd};        return sendCMD(buf);    }    public boolean sendCMD(byte[] cmd) {        try {            OutputStream outStream = clientSocket.getOutputStream();            outStream.write(cmd);            return true;        } catch (IOException e) {            Log.d("BLUETOOTH", e.getMessage());        }        return false;    }}
